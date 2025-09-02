9a51177b5720f799eeab3273ce384b71c8b070f0
Wambui <wambuikaranja78@gmail.com>
2025-09-02 14:21:43 +0300

    updates seed file with data

--- seed.py
+++ seed.py
@@ -1,8 +1,9 @@
 from crud import add_user,add_event,get_all_tickets,book_ticket
-import datetime
+from datetime import datetime
 
-add_user("Alice", "Wambui", 12345)
-add_event("Concert", datetime(2025, 9, 10, 18, 0), 2)
+
+#add_user("Alice", "Wambui", 12345)
+#add_event("Concert", datetime(2025, 9, 10, 18, 0), 2)
 
 book_ticket(1, 1)
 book_ticket(1, 1)
