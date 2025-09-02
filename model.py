8ad3d5c2c7931d09e58cb44ffc4dbdd3e3c462f7
Wambui <wambuikaranja78@gmail.com>
2025-09-02 11:16:42 +0300

    updates table tickets and events

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Integer, Column, String
+from sqlalchemy import Integer, Column, String , DateTime , ForeignKey
 from sqlalchemy.orm import declarative_base,relationship
+from sqlalchemy import datetime
 
 Base = declarative_base ()
 
@@ -12,3 +13,19 @@ class User(Base):
     phone = Column(Integer, nullble=False)
 
 
+class Event(Base): 
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    date = Column(DateTime, nullable=False)
+    capacity = Column(Integer, nullable=False)
+
+
+class Ticket(Base):
+    __tablename__ = "tickets"
+
+    id = Column(Integer,primary_key=True)
+    user_id = Column(Integer,ForeignKey("users.id")) 
+    event_id = Column(Integer, ForeignKey("events.id"))
+    booked_at = Column(DateTime, default=datetime.datetime.utcnow)   
