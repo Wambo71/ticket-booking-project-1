abb998478a585eceea379da3837b4f644d8097ac
Wambui <wambuikaranja78@gmail.com>
2025-09-02 14:10:06 +0300

    creates seed file

---
+++ seed.py
@@ -0,0 +1,10 @@
+from crud import add_user,add_event,get_all_tickets,book_ticket
+import datetime
+
+add_user("Alice", "Wambui", 12345)
+add_event("Concert", datetime(2025, 9, 10, 18, 0), 2)
+
+book_ticket(1, 1)
+book_ticket(1, 1)
+
+get_all_tickets()
